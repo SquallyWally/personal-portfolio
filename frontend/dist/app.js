d0cdaa4a22f97c04c54f860dd9c587e410b9cf07
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-11-01 21:18:59 +0100

    added visitor count

---
+++ frontend/dist/app.js
@@ -0,0 +1,8 @@
+function updateVisitorCount() {
+  let visitorCount = parseInt(localStorage.getItem("visitorCount")) || 0;
+  visitorCount++;
+  document.getElementById("visitor").innerHTML = visitorCount;
+  localStorage.setItem("visitorCount", visitorCount);
+}
+
+updateVisitorCount();
